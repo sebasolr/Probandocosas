db0bd12d0d754e0e3c0265c7e039f04f06a4866c
Sebastian Liberona <sebasolr.it@gmail.com>
2022-09-17 21:08:30 -0300

    Testeando Scraping

---
+++ index.js
@@ -0,0 +1,40 @@
+const puppeteer = require('puppeteer');
+
+
+(async () => {
+    //detrmina el buscador
+    const browser = await puppeteer.launch({ headless: false });
+    //me permite ingresar a nuevas paginas
+    const page = await browser.newPage();
+    //interactuar con diferentes paginas
+    await page.goto('https://www.falabella.com/falabella-cl');
+    //consigue una screenshot de la direccion
+    //await page.screenshot({ path: 'falabella1.jpg' });
+    //ingresamos el id del input y le pasamos la busqueda que queremos.
+    await page.type('#testId-SearchBar-Input','zapatillas');
+    //screenshot de la busqueda
+    //await page.screenshot({ path: 'falabella2.jpg' });
+    //ahora selecciono el el div que contiene al input o button, tomo su selector e indico donde haga click
+    await page.click('.SearchBar-module_searchBarWrapper__FWM7Z button');
+    // espero hasta que salga un selector que este en toda la pagina
+    await page.waitForSelector(`.search-results-2-grid`);
+    //hago screenshot de la busqueda.
+    //await page.screenshot({ path: 'falabella3.jpg' });
+    //dar un tiempo
+    //funcion anonima que inspecciona la pagina con js nativo.
+    const enlaces = await page.evaluate(() =>{
+        const elements = document.querySelectorAll('.search-results-2-grid div .pod-details-2_GRID a')
+        const links =[];
+        for (let element of elements){ 
+            links.push(element.href)
+        }return links
+    });
+
+    for(let enlace of enlaces){
+        await page.goto(enlace);
+    }
+   
+
+    await browser.close();
+
+})();
